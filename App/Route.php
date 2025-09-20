18723d6013524344c4f6357fd418c497c1d4cb9a
Rayssa Vicente <rayssavicenteviegas@gmail.com>
2025-09-20 17:56:11 -0300

    Arquivo de Rotas do projeto (responsável por mapear URLs para seus respectivos Controllers e Actions).

---
+++ App/Route.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App;
+
+use MF\Init\Bootstrap;
+
+class Route extends Bootstrap {
+
+	protected function initRoutes() {
+
+		$routes['home'] = array(
+			'route' => '/',
+			'controller' => 'indexController',
+			'action' => 'index'
+		);
+
+		$routes['inscreverse'] = array(
+			'route' => '/inscreverse',
+			'controller' => 'indexController',
+			'action' => 'inscreverse'
+		);
+
+		$routes['singup'] = array(
+			'route' => '/singup',
+			'controller' => 'indexController',
+			'action' => 'singup'
+		);
+
+		$routes['registrar'] = array(
+			'route' => '/registrar',
+			'controller' => 'indexController',
+			'action' => 'registrar'
+		);
+
+		$routes['autenticar'] = array(
+			'route' => '/autenticar',
+			'controller' => 'AuthController',
+			'action' => 'autenticar'
+		);
+
+	
+		$routes['time'] = array(
+			'route' => '/time',
+			'controller' => 'AppController',
+			'action' => 'time'
+		);
+		
+		$routes['sair'] = array(
+			'route' => '/sair',
+			'controller' => 'AuthController',
+			'action' => 'sair'
+		);
+
+		
+		$routes['tweet'] = array(
+			'route' => '/tweet',
+			'controller' => 'AppController',
+			'action' => 'tweet'
+		);
+
+		$routes['quem_seguir'] = array(
+			'route' => '/quem_seguir',
+			'controller' => 'AppController',
+			'action' => 'quemSeguir'
+		);
+
+		$routes['acao'] = array(
+			'route' => '/acao',
+			'controller' => 'AppController',
+			'action' => 'acao'
+		);
+
+		
+		$routes['remover_tweet'] = array(
+			'route' => '/remover',
+			'controller' => 'AppController',
+			'action' => 'removerTweet'
+		);
+
+
+
+		$this->setRoutes($routes);
+	}
+
+}
+
+?>
