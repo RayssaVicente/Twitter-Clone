b15ca914d298e0a2c54bb76caca6a5b495987bee
Rayssa Vicente da Silva Viegas <102470604+RayssaVicente@users.noreply.github.com>
2025-11-06 08:58:50 -0300

    refazendo o AuthControlller e verificando a eficiência das funções

--- App/Controllers/AuthController.php
+++ App/Controllers/AuthController.php
@@ -6,53 +6,36 @@ namespace App\Controllers;
 use MF\Controller\Action;
 use MF\Model\Container;
 
-
-class AuthController extends Action{
-
-    public function autenticar(){
-        $usuario = Container::getModel('Usuario');
-
-        $usuario->__set('email', $_POST['email']);
-        $usuario->__set('senha', md5($_POST['senha']));
-
-    
-         $retorno = $usuario->autenticar();
-
-         if($usuario->__get('id') != '' && $usuario->__get('nome')){
-           session_start();
-
-           $_SESSION['id'] = $usuario->__get('id');
-            $_SESSION['nome'] = $usuario->__get('nome');
-
-            header('Location: /time');
-         }else{
-            echo 'Erro na autenticação';
-            header('Location: /?login=erro');
-         }
-       
-    }
-
-    public function sair(){
-            session_start();
-            session_destroy();
-            header('Location: /');
-   }
-
-   public function remover() {
-    $query = "DELETE FROM tweets WHERE id = :id AND id_usuario = :id_usuario";
-    $stmt = $this->db->prepare($query);
-    $stmt->bindValue(':id', $this->__get('id'));
-    $stmt->bindValue(':id_usuario', $this->__get('id_usuario'));
-    $stmt->execute();
-
-    return $this;
-   }
+class AuthController extends Action {
 
 
+	public function autenticar() {
+		
+		$usuario = Container::getModel('Usuario');
 
+		$usuario->__set('email', $_POST['email']);
+		$usuario->__set('senha', md5($_POST['senha']));
+
+		$usuario->autenticar();
+
+		if($usuario->__get('id') != '' && $usuario->__get('nome')) {
+			
+			session_start();
+
+			$_SESSION['id'] = $usuario->__get('id');
+			$_SESSION['nome'] = $usuario->__get('nome');
+
+			header('Location: /timeline');
+
+		} else {
+			header('Location: /?login=erro');
+		}
+
+	}
+
+	public function sair() {
+		session_start();
+		session_destroy();
+		header('Location: /');
+	}
 }
-
-
-
-
-?>
