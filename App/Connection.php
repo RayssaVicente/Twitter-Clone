9ad95fea413c457130bc8c2c4b75c4679e871785
Rayssa Vicente da Silva Viegas <102470604+RayssaVicente@users.noreply.github.com>
2025-11-06 08:56:33 -0300

    Coneção com o banco de dados e tratamento de erro

--- App/Connection.php
+++ App/Connection.php
@@ -8,16 +8,17 @@ class Connection {
 		try {
 
 			$conn = new \PDO(
-				"mysql:host=localhost;dbname=twitter_clone;charset=utf8",
+				"mysql:host=localhost;dbname=twitter_clone;charset=utf8mb4;",
 				"root",
 				"0612" 
 			);
+			 $conn->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
 
 			return $conn;
 
 		} catch (\PDOException $e) {
-			//.. tratar de alguma forma ..//
-		}
+            die("Erro ao conectar ao banco: " . $e->getMessage());
+        }
 	}
 }
 
