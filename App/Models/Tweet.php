2c16b2e89f8b4dc7a95204c5f57542eb7d0aea7f
Rayssa Vicente da Silva Viegas <102470604+RayssaVicente@users.noreply.github.com>
2025-12-18 09:58:57 -0300

    Os models dos tweet dos usuário e da pesquisa dos usuários

--- App/Models/Tweet.php
+++ App/Models/Tweet.php
@@ -30,41 +30,66 @@ class Tweet extends Model{
         return $this;
     }
 
-    public function getAll(){
-        $query = " 
+    public function getAll() {
+
+		$query = "
+			select 
+				t.id, 
+				t.id_usuario, 
+				u.nome, 
+				t.tweet, 
+				DATE_FORMAT(t.data, '%d/%m/%Y %H:%i') as data
+			from 
+				tweets as t
+				left join usuarios as u on (t.id_usuario = u.id)
+			where 
+				t.id_usuario = :id_usuario
+				or t.id_usuario in (select id_usuario_seguindo from usuarios_seguidores where id_usuario = :id_usuario)
+			order by
+				t.data desc
+		";
+
+		$stmt = $this->db->prepare($query);
+		$stmt->bindValue(':id_usuario', $this->__get('id_usuario'));
+		$stmt->execute();
+
+		return $stmt->fetchAll(\PDO::FETCH_ASSOC);
+	}
+
+	public function remover() {
+    // Deleta o registro conferindo o ID do tweet e o ID do autor (segurança adicional)
+    $query = "delete from tweets where id = :id and id_usuario = :id_usuario";
+    $stmt = $this->db->prepare($query);
+    $stmt->bindValue(':id', $this->__get('id'));
+    $stmt->bindValue(':id_usuario', $this->__get('id_usuario'));
+    $stmt->execute();
+
+    return true;
+}
+
+public function getPorUsuario() {
+    $query = "
         select 
-            t.id, 
-            t.id_usuario, 
-            u.nome, 
-            t.tweet, DATE_FORMAT(t.data, '%d/%m/%Y %H:%i') as data
+            t.id, t.id_usuario, u.nome, t.tweet, DATE_FORMAT(t.data, '%d/%m/%Y %H:%i') as data
         from 
-        tweets  as t
-        left join usuarios as u on (t.id_usuario = u.id)
+            tweets as t
+            left join usuarios as u on (t.id_usuario = u.id)
         where 
-        t.id_usuario = :id_usuario
-        or t.id_usuario in (select id_usuario_seguindo from usuarios_seguidores where id_usuario = :id_usuario)
+            t.id_usuario = :id_usuario
         order by
             t.data desc
-        ";
+    ";
 
-        $stmt = $this->db->prepare($query);
-        $stmt->bindValue(':id_usuario', $this->__get('id_usuario'));
-        $stmt->execute();
+    $stmt = $this->db->prepare($query);
+    $stmt->bindValue(':id_usuario', $this->__get('id_usuario'));
+    $stmt->execute();
 
-        return $stmt->fetchAll(\PDO::FETCH_ASSOC);
-    }
-
-    public function remover(){
-        $query = "DELETE FROM tweets WHERE id = :id AND id_usuario = :id_usuario";
-        $stmt = $this->db->prepare($query);
-        $stmt->bindValue(':id', $this->__get('id'));
-        $stmt->bindValue(':id_usuario', $this->__get('id_usuario'));
-        $stmt->execute();
-
-        return $this;
-    }
+    return $stmt->fetchAll(\PDO::FETCH_ASSOC);
+}
 
 }
+
+
 
 
 ?>
