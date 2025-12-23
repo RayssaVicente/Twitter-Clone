da25823453167b0b200e8d17fd7adf0b42551d9c
Rayssa Vicente da Silva Viegas <102470604+RayssaVicente@users.noreply.github.com>
2025-12-23 16:57:21 -0300

    Fix: Sincronizando pastas fisicas com composer.json (PSR-4)

--- public/index.php
+++ public/index.php
@@ -2,7 +2,6 @@
 spl_autoload_register(function ($class) {
     $base_dir = __DIR__ . '/../';
     
-    // Converte exatamente o Namespace em Caminho (Ex: MF\Init\Bootstrap -> MF/Init/Bootstrap.php)
     $file = $base_dir . str_replace('\\', '/', $class) . '.php';
 
     if (file_exists($file)) {
